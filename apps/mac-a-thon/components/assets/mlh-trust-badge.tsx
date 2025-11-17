bb0f1125a7b8b1448679533a7176658ff49ded2b
Aidan Froggatt <aidanfr@live.ca>
2025-11-16 21:48:54 -0500

    updating assets for the header, mlh badge, etc.

--- apps/mac-a-thon/components/assets/mlh-trust-badge.tsx
+++ apps/mac-a-thon/components/assets/mlh-trust-badge.tsx
@@ -11,12 +11,12 @@ const MLHTrustBadge = () => {
         height: 'auto',
         zIndex: 10000,
       }}
-      href='https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white'
+      href='https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=blue'
       target='_blank'
       rel='noopener noreferrer'
     >
       <Image
-        src='https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg'
+        src='https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-blue.svg'
         alt='Major League Hacking 2026 Hackathon Season'
         width={50}
         height={60}
